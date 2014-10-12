d776f9b7148496b6b45e359cf115365bb9c9d0f7
Luc <luc.lebosse@gmail.com>
2014-10-12 18:24:55 +0800

    Add missing condition for FEATURE_BEEPER
    M300 was not disabled if FEATURE_BEEPER is set to 0

--- src/ArduinoDUE/Repetier/Commands.cpp
+++ src/ArduinoDUE/Repetier/Commands.cpp
@@ -1074,7 +1074,7 @@ void Commands::executeGCode(GCode *com)
             break;
 #endif
 
-#if defined(BEEPER_PIN) && BEEPER_PIN>=0
+#if defined(BEEPER_PIN) && BEEPER_PIN>=0 && FEATURE_BEEPER
         case 300:
         {
             int beepS = 1;
