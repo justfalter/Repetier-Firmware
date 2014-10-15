3fef0731766c8e1ec658ca2bb0943d9a6fb68680
Luc <luc.lebosse@gmail.com>
2014-10-15 13:42:47 +0800

    Fix Confirmation dialog page to allow title change
    In configuration dialog page title variable was not set so title was
    always "Please Confirm" instead of the parameter sent to function

--- src/ArduinoDUE/Repetier/uilang.h
+++ src/ArduinoDUE/Repetier/uilang.h
@@ -110,7 +110,7 @@
 #define UI_TEXT_PREHEAT_ABS      "Preheat ABS"
 #define UI_TEXT_LIGHTS_ONOFF     "Lights :%lo"
 #define UI_TEXT_SOUND_ONOF		 "Sound :%so"
-#define UI_TEXT_CONFIRMATION     "Please Confirm","%C2","%C3","%C4"
+#define UI_TEXT_CONFIRMATION     "%C1","%C2","%C3","%C4"
 #define UI_TEXT_YES_SELECTED     "  [Yes]    No"
 #define UI_TEXT_NO_SELECTED     "   Yes    [No]"
 #define UI_TEXT_PLEASE_CONFIRM     "  Please confirm"
